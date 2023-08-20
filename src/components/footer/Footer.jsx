2d6239d7a96a44f202c0f0bbddc10134f3a6100d
T T <tomergk10@gmail.com>
2023-08-20 18:02:42 +0300

    updated stuff

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -25,11 +25,10 @@ const Footer = () => {
       
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="#music">Music</a></li>
-        <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#projects2">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
+        <li><a href="#music">Music</a></li>
       </ul>
 
       <div className="footer__socials">
